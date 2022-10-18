787ae858b004b1c08a62642b67401b3303f81cd7
Jan Brederecke <58463668+janbrederecke@users.noreply.github.com>
2022-10-18 17:32:24 +0200

    Finished create_data function

--- create_data.py
+++ create_data.py
@@ -10,8 +10,22 @@ def create_data(size):
     be trained as an adding machine
     """
 
-    train_data = np.random.randint(-10, 10, size)
-    test_data = np.random.randint(-10, 10, int(0.5 * size))
-    holdout_data = np.random.randint(-10, 10, int(0.5 * size))
+    # train_data
+    train_data = np.array([np.random.randint(-10, 10, size),
+                            np.random.randint(-10, 10, size)]).T
+    row_sum = np.sum(train_data, axis = 1)[np.newaxis,:].T
+    train_data = np.hstack((train_data, row_sum))
+
+    # test_data
+    test_data = np.array([np.random.randint(-10, 10, int(size * 0.5)),
+                            np.random.randint(-10, 10, int(size * 0.5))]).T
+    row_sum = np.sum(test_data, axis = 1)[np.newaxis,:].T
+    test_data = np.hstack((test_data, row_sum))
+
+    # holdout_data
+    holdout_data = np.array([np.random.randint(-10, 10, int(size * 0.5)),
+                            np.random.randint(-10, 10, int(size * 0.5))]).T
+    row_sum = np.sum(holdout_data, axis = 1)[np.newaxis,:].T
+    holdout_data = np.hstack((holdout_data, row_sum))
 
     return train_data, test_data, holdout_data
